c0471c65ce411e56a7167c9392cb2ea6864e1d3b
wesleydearaujoteixeira <145611326+wesleydearaujoteixeira@users.noreply.github.com>
2023-10-13 00:29:49 -0300

    Exercicio de repetição

--- Exercício/Estrutura de repetição/contagem.js
+++ Exercício/Estrutura de repetição/contagem.js
@@ -3,14 +3,22 @@ function contar(){
     var termo = Number(document.body.querySelector('#inicio').value)
     var r = Number(document.body.querySelector('#passo').value)
     var fim = Number(document.body.querySelector('#fim').value)
+    im2 = fim + 1
     var contagem = document.body.querySelector('#res')
-    while(termo <= fim){
-        contagem.innerHTML += ` ${termo} `
-        termo += r 
-        
-
+    if(r == 0 || termo == 0 || fim == 0 ){
+    window.alert('Erro! Tente Novamente!')
+    }else if (fim > termo){
+        while(termo < fim){
+            contagem.innerHTML += `👉${termo} `
+            termo += r  
+    }contagem.innerHTML += `🏁 `
+    
+    }else if (termo > fim){
+        while(termo > fim){
+            contagem.innerHTML += `👉${termo} `
+            termo -= r  
+    }contagem.innerHTML += `🏁 `
+   
     }
 
-    
-    
 }
